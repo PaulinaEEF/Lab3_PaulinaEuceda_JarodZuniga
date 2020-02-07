b102166653b04df19cabd238412604418670881a
PaulinaEEF <paulinaeuceda@gmail.com>
2020-02-07 15:18:46 -0600

    Main creado exitosamente

--- src/lab3_jarodzuniga_paulinaeuceda/Lab3_JarodZuniga_PaulinaEuceda.java
+++ src/lab3_jarodzuniga_paulinaeuceda/Lab3_JarodZuniga_PaulinaEuceda.java
@@ -1,19 +1,78 @@
 package lab3_jarodzuniga_paulinaeuceda;
 
+import java.util.Scanner;
+
 public class Lab3_JarodZuniga_PaulinaEuceda {
+    static Scanner lea = new Scanner(System.in);
 
     public static void main(String[] args) {
         // TODO code application logic here
+        char resp = 's';
+        while (resp == 's') {
+            System.out.println("1) Crear equipo");
+            System.out.println("2) Despedir jugador");
+            System.out.println("3) Despedir medico");
+            System.out.println("4) Despedir entrenador");
+            System.out.print("Eliga la opcion que desea: ");
 
-        System.out.println("m");
-        
-        for (int i = 0; i < 10; i++) {
-            
+            String nombrePer, apellido, colegioMed, ciudadNatal, jugadaFav;
+            int años, salario, numcamiseta, tiro3, defensa, tiroMedia, rebote, bandeja, pases, posteo, altura;
+            int cantCiru, cantEnf, cantTer, valorDueño;
+            boolean Fue;
+
+            String nomEq, descripcion;
+            int año, catCampeo, porcentaje;
+
+            int opcion = lea.nextInt();
+            System.out.println("");
+            switch (opcion) {
+                case 1:
+                    char salida = 's';
+                    while (salida == 's') {
+                        System.out.println("1) Crear jugador");
+                        System.out.println("2) Crear dueño");
+                        System.out.println("3) Crear entrenador principal");
+                        System.out.println("4) Crear asistente");
+                        System.out.println("5) Crear medico");
+                        System.out.println("6) Crear preparador fisico");
+                        System.out.print("Ingrese la opcion que desea: ");
+                        int op = lea.nextInt();
+
+                        System.out.println("");
+                        switch (op) {
+                            case 1:
+                                System.out.print("Ingrese el nombre del jugador: ");
+                                nombrePer = lea.next();
+
+                                System.out.print("Ingrese apellido: ");
+                                apellido = lea.next();
+
+                                System.out.print("Ingrese años como profesional: ");
+                                años = lea.nextInt();
+
+                                System.out.print("Ingrese el salario del jugador: ");
+                                salario = lea.nextInt();
+
+                                System.out.println("");
+                                break;
+
+                            case 2:
+                                System.out.print("Ingrese el valor neto del dueño: ");
+
+                                break;
+                            default:
+                                throw new AssertionError();
+                        }
+
+                    }
+                    break;
+                default:
+
+                    break;
+
+            }
+
         }
-        
-
-        System.out.println("mo");
-        System.out.println("hola");
     }
-    
+
 }
