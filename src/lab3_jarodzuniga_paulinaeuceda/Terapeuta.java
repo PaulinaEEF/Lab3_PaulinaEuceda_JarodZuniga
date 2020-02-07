fb9dd9c3db9fcd3df5ce228d1c5a141b7b50577a
Rosalba <JJJZuniga>
2020-02-07 15:14:05 -0600

    Calses con atributos creados

--- src/lab3_jarodzuniga_paulinaeuceda/Terapeuta.java
+++ src/lab3_jarodzuniga_paulinaeuceda/Terapeuta.java
@@ -10,5 +10,26 @@ package lab3_jarodzuniga_paulinaeuceda;
  * @author Rosa
  */
 public class Terapeuta {
+    private int num_terapias;
+
+    public Terapeuta() {
+    }
+
+    public Terapeuta(int num_terapias) {
+        this.num_terapias = num_terapias;
+    }
+
+    public int getNum_terapias() {
+        return num_terapias;
+    }
+
+    public void setNum_terapias(int num_terapias) {
+        this.num_terapias = num_terapias;
+    }
+
+    @Override
+    public String toString() {
+        return "Terapeuta{" + "num_terapias=" + num_terapias + '}';
+    }
     
 }
